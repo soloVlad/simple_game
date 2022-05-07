d2e4236d3e1edfa82e7cdeba3f4adeb8c8687716
Vlad Solovey <connectwithvlad@gmail.com>
2022-05-07 11:50:05 +0300

    feat: add first working version of the game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private AudioSource playerAudio;
+    public AudioClip shootSound;
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 20f;
+
+    public GameObject projectilePrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Making bounds for player movement
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        // Move the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            // Play a sound
+            playerAudio.PlayOneShot(shootSound, 1.0f);
+        }
+    }
+}
